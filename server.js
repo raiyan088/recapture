9ca3a5b05d90b56b712e51dae99f1551b88120d0
raiyan088 <78313577+raiyan088@users.noreply.github.com>
2022-02-19 13:35:18 +0600

    Add files via upload

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const poll = require('promise-poller').default
 const puppeteer = require('puppeteer-extra')
 const express = require('express')
 const http = require('http')
+const fs = require('fs')
 
 let app = express()
 
@@ -20,90 +21,37 @@ server.listen(process.env.PORT || 3000, ()=>{
 ;(async () => {
 
     browser = await puppeteer.launch({
-        headless: true,
-        defaultViewport: null,
-        slowMo:10,
+        headless: false,
         args: ['--no-sandbox', '--disable-setuid-sandbox']
     })
 
     page = (await browser.pages())[0]
 
     const google = '6Le-wvkSAAAAAPBMRTvw0Q4Muexq9bi0DJwx_mJ-'
-    
-    //await page.goto('https://www.google.com/recaptcha/api2/demo', {waitUntil: 'networkidle2', timeout: 0})
+  
+    /*await page.evaluate(() => {
+      const div = document.createElement("div")
+      div.setAttribute("class", "c-form-group g-recaptcha")
+      div.setAttribute("data-sitekey", "6LeTnxkTAAAAAN9QEuDZRpn90WwKk_R1TRW_g-JC")
+      document.body.appendChild(div)
+    })*/
+    await page.goto('https://www.google.com/search?q=my+ip+address&sxsrf=APq-WBsMMG1Z_RkNGVLIX6eTsc8P694IZg%3A1645255923757&ei=85wQYv_0LdqaseMPjY-iwAg&oq=my+i&gs_lcp=Cgdnd3Mtd2l6EAEYADIHCAAQsQMQQzIICAAQsQMQkQIyBAgAEEMyBwgAELEDEEMyBAgAEEMyBAgAEEMyBAgAEEMyCAgAEIAEELEDMggIABCABBCxAzIICC4QgAQQ1AI6BwgAEEcQsAM6BwgAELADEEM6BAgjECc6BQgAEIAEOgoIABCABBCHAhAUOgcIIxDqAhAnOgsIABCABBCxAxCDAToFCAAQkQJKBAhBGABKBAhGGABQjgdYzh5goS1oA3ABeASAAd4BiAH6DpIBAzItOZgBAKABAbABCsgBCsABAQ&sclient=gws-wiz', {waitUntil: 'networkidle2', timeout: 0})
 
-    console.log('Solved')
-    /*const requestId = await initiateCaptchaRequest()
+    await page.screenshot({ path: "./image.png" })
 
-    console.log(requestId)
-
-    const response = await pollForRequestResults(requestId);
-
-    console.log(response)
-
-    await page.evaluate(`document.getElementById("g-recaptcha-response").innerHTML="${response}";`);
-*/
-    //await page.click('#register-form button[type=submit]');
 })()
 
 
-app.get('/recapture', function(req, res) {
-  
-    if(req.query) {
-        const key = req.query.key
-        if(key) {
-            const html = '<html>'
-                        +'  <head>'
-                        +'    <title>reCAPTCHA demo: Simple page</title>'
-                        +'    <script src="https://www.google.com/recaptcha/api.js" async defer></script>'
-                        +'  </head>'
-                        +'  <body>'
-                        +'     <div class="g-recaptcha" data-sitekey="'+key+'"></div>'
-                        +'  </body>'
-                        +'</html>'
-            res.send(html)
-        } else {
-            res.send('error')
-        }
-    } else{
-        res.send('error')
-    }
+app.get('/image', function(req, res) {
+    fs.readFile('./image.png', function (err, data) {
+      if (err) {
+          res.writeHeader(400, {"Content-Type": "text/html"});  
+          res.write('error');  
+          res.end();
+      }else {
+          res.writeHeader(200, {"Content-Type": "image/png"});  
+          res.write(data);  
+          res.end();
+      }
+  })
 })
-
-
-async function initiateCaptchaRequest(siteKey) {
-    const formData = {
-      method: 'userrecaptcha',
-      googlekey: siteKey,
-      key: '72840c3f39a6cdac651f594bec54969d',
-      pageurl: 'https://old.reddit.com/login',
-      json: 1
-    };
-    const response = await request.post('http://2captcha.com/in.php', {form: formData});
-    return JSON.parse(response).request;
-}
-
-async function pollForRequestResults(id, retries = 30, interval = 1500, delay = 15000) {
-    await timeout(delay);
-    return poll({
-      taskFn: requestCaptchaResults('72840c3f39a6cdac651f594bec54969d', id),
-      interval,
-      retries
-    });
-}
-
-
-function requestCaptchaResults(apiKey, requestId) {
-    const url = `http://2captcha.com/res.php?key=${apiKey}&action=get&id=${requestId}&json=1`;
-    return async function() {
-      return new Promise(async function(resolve, reject){
-        const rawResponse = await request.get(url);
-        const resp = JSON.parse(rawResponse);
-        console.log(resp)
-        if (resp.status === 0) return reject(resp.request);
-        resolve(resp.request);
-      });
-    }
-  }
-  
-const timeout = millis => new Promise(resolve => setTimeout(resolve, millis))
